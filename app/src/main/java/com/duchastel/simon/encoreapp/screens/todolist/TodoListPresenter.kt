c5802d5330f79d9a6998dca747eac79d3e2a1ce7
Simon Duchastel <simon@duchastel.com>
2025-04-06 16:41:37 -0400

    Add basic ui

--- app/src/main/java/com/duchastel/simon/encoreapp/screens/todolist/TodoListPresenter.kt
+++ app/src/main/java/com/duchastel/simon/encoreapp/screens/todolist/TodoListPresenter.kt
@@ -1,53 +1,29 @@
 package com.duchastel.simon.encoreapp.screens.todolist
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import com.duchastel.simon.encoreapp.data.TodoItemRepository
+import com.duchastel.simon.encoreapp.utils.Async
 import com.slack.circuit.codegen.annotations.CircuitInject
 import com.slack.circuit.runtime.presenter.Presenter
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Inject
 
 @CircuitInject(TodoListScreen::class, SingletonComponent::class)
-class TodoListPresenter @Inject constructor() : Presenter<TodoListScreen.State> {
+class TodoListPresenter @Inject constructor(
+    private val todoItemRepository: TodoItemRepository
+) : Presenter<TodoListScreen.State> {
     @Composable
     override fun present(): TodoListScreen.State {
-        var todoItems by remember {
-            mutableStateOf(
-                listOf(
-                    TodoItem(
-                        id = "1",
-                        text = "Learn Compose",
-                        isCompleted = true
-                    ),
-                    TodoItem(
-                        id = "2",
-                        text = "Build an Encore app",
-                        isCompleted = false
-                    ),
-                    TodoItem(
-                        id = "3",
-                        text = "Publish to Play Store",
-                        isCompleted = false
-                    )
-                )
-            )
-        }
+        val todoItems by todoItemRepository.getTodoItems().collectAsState(initial = null)
 
         return TodoListScreen.State(
-            todoItems = todoItems,
+            todoItems = todoItems?.let { Async.Success(it) } ?: Async.Loading(),
             emitEvent = { event ->
                 when (event) {
                     is TodoListScreen.Event.TodoItemCheckedChanged -> {
-                        todoItems = todoItems.map { item ->
-                            if (item.id == event.id) {
-                                item.copy(isCompleted = event.isCompleted)
-                            } else {
-                                item
-                            }
-                        }
+                        todoItemRepository.updateTodoItem(event.id, event.isCompleted)
                     }
                 }
             }
--- app/src/main/java/com/duchastel/simon/encoreapp/screens/todolist/TodoListScreen.kt
+++ app/src/main/java/com/duchastel/simon/encoreapp/screens/todolist/TodoListScreen.kt
@@ -1,5 +1,7 @@
 package com.duchastel.simon.encoreapp.screens.todolist
 
+import androidx.compose.runtime.Immutable
+import com.duchastel.simon.encoreapp.utils.Async
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
 import com.slack.circuit.runtime.screen.Screen
@@ -7,13 +9,17 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data object TodoListScreen: Screen {
+    @Immutable
     data class State(
-        val todoItems: List<TodoItem> = emptyList(),
+        val todoItems: Async<List<TodoItem>>,
         val emitEvent: (Event) -> Unit,
     ) : CircuitUiState
 
     sealed interface Event : CircuitUiEvent {
-        data class TodoItemCheckedChanged(val id: String, val isCompleted: Boolean) : Event
+        data class TodoItemCheckedChanged(
+            val id: String,
+            val isCompleted: Boolean,
+        ) : Event
     }
 }
 
